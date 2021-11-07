ea966b90e3dbfe2f21e856e8f0605d1ea19a63ff
dzakybrori <broridzaky2702@gmail.com>
2021-11-07 14:26:23 +0700

    Add font awesome package and extracted some widget from input page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './extensions.dart';
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -12,9 +14,51 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text('BMI CALCULATOR')),
-      body: const Center(child: Text('Body Text')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: CustomCard()),
+                Expanded(child: CustomCard()),
+              ],
+            ),
+          ),
+          Expanded(child: CustomCard()),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: CustomCard()),
+                Expanded(child: CustomCard()),
+              ],
+            ),
+          ),
+        ],
+      ),
       floatingActionButton:
           FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final Color? color;
+  final Widget? child;
+
+  const CustomCard({
+    Key? key,
+    this.color,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(context.dp(14)),
+      decoration: BoxDecoration(
+          color: color ?? context.background,
+          borderRadius: BorderRadius.circular(10)),
+      child: child,
     );
   }
 }
